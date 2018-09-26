c42d6e5ba852d2f3b081de8dae414621e075d230
RafaSojo <rafasojo98@gmail.com>
2018-09-26 09:59:22 +0200

    Initial upload
    Upload initial files and exercises 1 and 2

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 DWEC
+
+Repositorio para la asignatura de Desarrollo Web en Entorno Cliente.
---
+++ Tema 2/Ejercicio 2/codigo.js
@@ -0,0 +1,2 @@
+mensaje = "Hola Mundo!\nQué fácil es incluir 'comillas simples'\ny \"comillas dobles\""
+alert(mensaje);
