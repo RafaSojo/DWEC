59fe5c6c00a9b7d98a062ed3c4e9137b5ae627df
RafaSojo <rafasojo98@gmail.com>
2018-10-05 12:16:20 +0200

    updated ej14

--- tema2/ejercicio14/codigo.js
+++ tema2/ejercicio14/codigo.js
@@ -3,21 +3,21 @@
     function init(){
         let enlaces = document.getElementsByTagName('a');
         for(i=0;i<enlaces.length;i++)
-          document.getElementById(enlaces[i].id).addEventListener('click', muestraOculta);        
+          enlaces[i].addEventListener('click', muestraOculta);
     }
     function muestraOculta() {
         let parrafo = document.getElementById('contenidos_' + this.id);
         switch(parrafo.style.display) {
           case 'none':
             parrafo.style.display = 'block';
-            this.innerHTML = 'Ocultar contenidos';
+            this.textContent = 'Ocultar contenidos';
             break;
           case 'block':
           case '':
             parrafo.style.display = 'none';
-            this.innerHTML = 'Mostrar contenidos';
+            this.textContent = 'Mostrar contenidos';
             break;
-        }
+        } 
       }
 }
 
