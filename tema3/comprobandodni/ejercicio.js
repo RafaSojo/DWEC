9a204eb39a0a8d0236843546f47794c92f9bd04b
RafaSojo <rafasojo98@gmail.com>
2018-11-03 23:54:04 +0100

    ej DNI

---
+++ tema3/comprobandodni/ejercicio.js
@@ -0,0 +1,43 @@
+{
+    
+    function init(){
+        document.getElementById('dni').addEventListener('focusout',comprobarDni);
+    }
+    
+    
+    function comprobarDni(){
+        let valor = this.value;
+        
+        if (valor == ''){
+            mensajeAlerta('Tienes que introducir algo');
+            return
+        }
+        
+        let regexSimple = /^([0-9]{8})([a-z])$/i;
+        
+        let resultado = regexSimple.exec(valor);
+        
+        if(resultado == null){
+            mensajeAlerta('Formato de dni inválido');
+            return;
+        }
+        
+        let letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
+        
+        let letraCalculada = letras[parseInt(resultado[1]) % 23];
+        let letraTexto = valor.substring(8,9).toString();
+        
+        if(letraCalculada != letraTexto.toUpperCase()){
+            mensajeAlerta('La letra del DNI no es correcta');
+            return;
+        }
+        mensajeAlerta('');
+    }
+    
+    function mensajeAlerta(mensaje){
+        let spanAlerta = document.getElementById('errorDni'); // Temporalmente en esta función; habría que sacarlo fuera
+        spanAlerta.innerText = mensaje;
+    }
+    
+    window.addEventListener("load", init);
+}
