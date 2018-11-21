8800a74505e1b45e2662df8becc09a38a4edc0d8
RafaSojo <rafasojo98@gmail.com>
2018-11-21 08:47:21 +0100

    Examen sin corregir

---
+++ tema3/examen/ejercicio1.js
@@ -0,0 +1,46 @@
+{
+    function init() {
+        // Para ir para atrás
+        document.getElementById('atras').addEventListener('click', irAtras);
+
+        // Para resetear
+        document.getElementById('resetear').addEventListener('click', resetear);
+
+        incrementarVisita();
+
+        let visita = window.localStorage.getItem('contador');
+
+        escribirMensaje(generaMensaje(visita));
+    }
+
+    function generaMensaje(visita) {
+        if (visita == null || visita == 1)
+            return "Bienvenido a mi humilde morada. Esta es la primera vez que entras. Espero que vuelvas.";
+        if (visita == 2)
+            return "Hola de nuevo. Ya estás aquí por segunda vez. ¿Volveremos a vernos?";
+        return "Ya empiezas a ser pesado. Esta es la vez número " + visita + " que entras. Sigue con tus cosas";
+    }
+
+    function resetear() {
+        window.localStorage.setItem('contador', 0);
+        escribirMensaje("RESETEADO");
+    }
+
+    function incrementarVisita() {
+        let visita = window.localStorage.getItem('contador');
+        if (visita == null || visita == 0)
+            window.localStorage.setItem('contador', 1);
+        else
+            window.localStorage.setItem('contador', Number(visita) + 1);
+    }
+
+    function escribirMensaje(mensaje){
+        document.getElementById('mensaje').innerText = mensaje;
+    }
+
+    function irAtras(){
+        window.history.back();
+    }
+
+    window.addEventListener('load', init);
+}
---
+++ tema3/examen/ejercicio23.js
@@ -0,0 +1,61 @@
+{
+    var set = new Set();
+    function init() {
+        // Para ir para atrás
+        document.getElementById('atras').addEventListener('click', window.history.back);
+
+        document.getElementById('nombreApellidos').addEventListener('blur', compruebaInput);
+    }
+
+    function compruebaInput(){
+        let elementoNombre = document.getElementById('nombre');
+        let elementoApellidos = document.getElementById('apellidos');
+
+        // Resetear inputs
+        elementoNombre.innerText = "";
+        elementoApellidos.innerText = "";
+        const expresion = /([A-ZÁÉÍÓÚÑ ]+), ([A-ZÁÉÍÓÚÑ ]+)/i;
+
+        let nombre,apellidos;
+
+        let texto = document.getElementById('nombreApellidos').value;
+
+        let mensajeError = "";
+        try{
+            if(expresion.test(texto) == false){
+                // lanzamos la excepcion
+                // raise Exception;
+                 mensajeError = "Error de formato";
+
+            }
+            else{
+
+                // TO-DO destructuring
+                resultados = expresion.exec(texto);
+                completo = resultados[0];
+                nombre = resultados[2];
+                apellidos = resultados[1];
+                if(set.has(completo) == true){
+        
+                    // lanzariamos exception
+                    mensajeError = "REPETIDO";
+                }else
+                {
+                    elementoNombre.innerText = nombre;
+                    elementoApellidos.innerText = apellidos;
+                    set.add(completo);
+                }
+            }
+        }
+        catch{
+            mensajeError = "Error de formato";
+        }
+
+        document.getElementById('mensajeError').innerText = mensajeError;
+
+    }
+
+ 
+
+    window.addEventListener('load', init);
+}
