345f7b3613ac6ff348fdd66d4756d334f7682ba6
RafaSojo <rafasojo98@gmail.com>
2018-11-25 11:37:46 +0100

    Parte 1 propuesta de examen

---
+++ tema3/examenCreadoPorMi/index.js
@@ -0,0 +1,16 @@
+{
+    function init() {
+        document.getElementById('enlace1').addEventListener('click', abrir1);
+        document.getElementById('enlace2').addEventListener('click', abrir2);
+    }
+
+    function abrir1(){
+        window.open('pass.html','_self');
+    }
+
+    function abrir2(){
+        window.open('.html','_self');
+    }
+
+    window.addEventListener('load', init);
+}
---
+++ tema3/examenCreadoPorMi/pass.js
@@ -0,0 +1,50 @@
+{
+    const expMayusculas = /[A-Z]+/g;
+    const expMinusculas = /[a-z]+/g;
+    const expCaracteres = /[^a-zA-Z0-9]+/g;
+    const expNumeros = /[0-9]+/g;
+    let spanMensaje;
+
+    function init() {
+        spanMensaje = document.getElementById('mensaje');
+        document.getElementById('atras').addEventListener('click', irAtras);
+        document.getElementById('password').addEventListener('blur', compruebaClave);
+    }
+
+    function compruebaClave() {
+        // this ahora representa el objeto que lo ha llamado. Es decir el input.
+        let clave = this.value;
+        let puntuacion = 0;
+
+        try {
+            if (clave.length < 8) {
+                throw new Error("La clave tiene que tener mínimo 8 caracteres.")
+            }
+
+            if (expMayusculas.test(clave)) {
+                puntuacion++;
+            }
+            if (expMinusculas.test(clave)) {
+                puntuacion++;
+            }
+            if (expCaracteres.test(clave)) {
+                puntuacion++;
+            }
+            if (expNumeros.test(clave)) {
+                puntuacion++;
+            }
+            spanMensaje.innerText = `La contraseña tiene una puntuación de ${puntuacion} de 4`;
+        } catch (error) {
+            spanMensaje.innerText = error.message;
+        }
+
+
+    }
+
+    function irAtras(event) {
+        event.preventDefault()
+        window.history.back();
+    }
+
+    window.addEventListener('load', init);
+}
