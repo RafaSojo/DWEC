c2069c37a0a5ca0d1063054fa52c1eae4e07d4e0
RafaSojo <rafasojo98@gmail.com>
2018-12-02 16:57:02 +0100

    [gatito] Actualizada estructura muy básica

--- tema4/gatito/gato.js
+++ tema4/gatito/gato.js
@@ -9,6 +9,21 @@
 
 
     let gato = {
+        // Campos:
+        nombre: "Juan", // To-Do: nombre aleatorio
+        peso: null,
+        vivo: true,
+        PESO_MAXIMO: 15,
+        PESO_MINIMO: 2,
+
+        comer(){
+            // To-Do: sumar peso
+        },
+
+        jugar(){
+            // To-Do: jugar
+        }
+        
         
 
     };
