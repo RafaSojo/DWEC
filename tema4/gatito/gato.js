b7e4576948ed29e4ad661a8eb80cacc87cb43cb6
RafaSojo <rafasojo98@gmail.com>
2018-12-05 17:11:35 +0100

    [gato] Añadida  estructura y comportamiento

--- tema4/gatito/gato.js
+++ tema4/gatito/gato.js
@@ -1,36 +1,70 @@
 {
-    function init(){
-        document.getElementById('optionComer').addEventListener('selected', comerGato);
-        
+  let gato;
+
+  function init() {
+    document.getElementById('optionComer').addEventListener('selected', comerGato);
+    document.getElementById('optionJugar').addEventListener('selected', jugarGato);
+    document.getElementById('optionDormir').addEventListener('selected', dormirGato);
+
+    gato = new Gato('Juan', '15/08/2018', 'Persa', 8);
+
+  }
 
 
 
-    }
+  // Creamos el contructor de Gato
+  function Gato(nombre, fechaNacimiento, raza, peso) {
+    this.nombre = nombre;
+    this.fechaNacimiento = fechaNacimiento;
+    this.raza = raza;
+    this.peso = peso;
+  }
+
+  // Ahora empezamos a ponerle los campos y los métodos
+
+  Gato.prototype.isVivo = true;
+  Gato.prototype.isDurmiendo = false;
+  Gato.prototype.PESO_MAXIMO = 15;
+  Gato.prototype.PESO_MINIMO = 2;
+
+  Gato.prototype.comer = function () {
+    this.incrementarPeso();
+  };
+
+  Gato.prototype.incrementarPeso = function () {
+    this.peso += 1;
+    if (peso > this.PESO_MAXIMO)
+      morir();
+  };
+
+  Gato.prototype.decrementarPeso = function () {
+    this.peso -= 1;
+    if (peso < this.PESO_MINIMO)
+      morir();
+  }
+
+  Gato.prototype.morir = function () {
+    this.isVivo = false;
+  };
+
+  Gato.prototype.dormir = function () {
+    this.isDurmiendo = true;
+  }
 
 
-    let gato = {
-        // Campos:
-        nombre: "Juan", // To-Do: nombre aleatorio
-        peso: null,
-        vivo: true,
-        PESO_MAXIMO: 15,
-        PESO_MINIMO: 2,
 
-        comer(){
-            // To-Do: sumar peso
-        },
+  function comerGato() {
+    gato.comer();
+  }
 
-        jugar(){
-            // To-Do: jugar
-        }
-        
-        
+  function jugarGato() {
+    gato.jugar();
+  }
 
-    };
+  function dormirGato() {
+    gato.dormir();
+  }
 
-    function comerGato(){
 
-    }
-
-    window.addEventListener('click', init);
+  window.addEventListener('click', init);
 }
