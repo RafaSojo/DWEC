3ed79c23acc3e6d221d5c7fd79070da3802a9b81
RafaSojo <rafasojo98@gmail.com>
2018-12-11 19:18:38 +0100

    [eventos-ratón] Añadida primera versión

---
+++ tema5/eventos-raton/index.js
@@ -0,0 +1,50 @@
+{
+    // No se porqué pero es sospechosamente parecido a otro código...
+    let canvas;
+    let arrayColores = ["#7E57C2", "#5C6BC0", "#42A5F5", "#EC407A", "#66BB6A", "#FF7043", "#8D6E63", "#78909C", "#004D40", "#1B5E20", "#33691E", "#E65100", "#263238", "#3E2723", "#880E4F"];
+
+    function init() {
+
+        // Para desactivar el menú contextual
+        document.getElementById('auxclick').addEventListener('contextmenu', (e) => {
+            e.preventDefault()
+        });
+        canvas = Array.from(document.getElementsByTagName("canvas"));
+
+
+        canvas.forEach(element => {
+            element.addEventListener(element.getAttribute("id"), ev => {
+                let aleatorio = Math.floor(Math.random() * (arrayColores.length - 0)) + 0;
+                pintaCanvas(element, aleatorio, ev.offsetX, ev.offsetY, ev.button, ev.buttons);
+            });
+            pintaCanvas(element);
+        });
+
+    }
+
+    let pintaCanvas = function (canvas, aleatorio, x, y, button, buttons) {
+        if (canvas.getContext) {
+            let ctx = canvas.getContext('2d', {
+                alpha: false
+            });
+            if (arguments.length === 1)
+                ctx.fillStyle = "#3D5AFE";
+            else
+                ctx.fillStyle = arrayColores[aleatorio];
+
+            ctx.fillRect(0, 0, 400, 400);
+            ctx.font = "bold 1.3rem Lato";
+            ctx.fillStyle = "#fff";
+            ctx.fillText(canvas.getAttribute("id"), 50, 30);
+
+            if (arguments.length > 1) {
+                ctx.font = "bold 1.1rem Lato";
+                ctx.fillText("x = " + x, 210, 40);
+                ctx.fillText("y = " + y, 210, 70);
+                ctx.fillText("button = " + button, 190, 100);
+                ctx.fillText("buttons = " + buttons, 190, 130);
+            }
+        }
+    }
+    window.addEventListener("load", init);
+}
