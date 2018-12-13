0ecc3e64294950afb9c41a2f27b48a8c54a67a6a
RafaSojo <rafasojo98@gmail.com>
2018-12-13 16:59:07 +0100

    [factura] Creadas clases

--- tema4/factura/clases.js
+++ tema4/factura/clases.js
@@ -1,19 +1,41 @@
-function Cliente(){
-
+function Cliente(nombre, cif, direccion, telefono, email) {
+    this.nombre = nombre;
+    this.cif = cif;
+    this.direccion = direccion;
+    this.email = email;
+    this.telefono = telefono;
 }
 
-function Linea(descripcionProducto, unidades, precio, iva, importe){
-    this.descripcionProducto = producto;
+function Linea(descripcionProducto, unidades, precio, iva, importe) {
+    this.descripcionProducto = descripcionProducto;
     this.unidades = unidades;
     this.precio = precio;
     this.iva = iva;
     this.importe = importe;
 }
 
-function Emisor(){
-
+function Emisor(nombre, cif, direccion, telefono, email) {
+    this.nombre = nombre;
+    this.cif = cif;
+    this.direccion = direccion;
+    this.telefono = telefono;
+    this.email = email;
 }
 
-function Factura(){
-
+function Factura(fecha, moneda, emisor, cliente, lineas, observaciones) {
+    this.fecha = fecha;
+    this.moneda = moneda;
+    this.emisor = emisor;
+    this.cliente = cliente;
+    this.lineas = lineas;
+    this.observaciones = observaciones;
+    this.total = this.calculaTotal();
+    this.id = this.incrementaId();
 }
+
+Factura.prototype.id = 0;
+
+Factura.prototype.incrementaId = function () {
+    return this.id++;
+};
+
