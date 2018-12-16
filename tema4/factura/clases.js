589e7aa558524368fbe7d094bf38e49db85f6ddf
RafaSojo <rafasojo98@gmail.com>
2018-12-16 12:05:09 +0100

    [factura] Versión funcional

--- tema4/factura/clases.js
+++ tema4/factura/clases.js
@@ -1,52 +1,49 @@
-function Cliente(nombre, cif, direccion, telefono, email) {
-    this.nombre = nombre;
-    this.cif = cif;
-    this.direccion = direccion;
-    this.email = email;
-    this.telefono = telefono;
+{
+    function Cliente(nombre, cif, direccion, telefono, email) {
+        this.nombre = nombre;
+        this.cif = cif;
+        this.direccion = direccion;
+        this.email = email;
+        this.telefono = telefono;
+    }
+
+    function Linea(descripcionProducto, unidades, precio, iva) {
+        this.descripcionProducto = descripcionProducto;
+        this.unidades = unidades;
+        this.precio = precio.toFixed(2);
+        this.iva = iva;
+        this.importe = ((precio * unidades) + ((precio * unidades) * iva / 100)).toFixed(2);
+    }
+
+    function Emisor(nombre, cif, direccion, telefono, email) {
+        this.nombre = nombre;
+        this.cif = cif;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    function Factura(fecha, moneda, emisor, cliente, lineas, observaciones) {
+        this.fecha = fecha;
+        this.moneda = moneda;
+        this.emisor = emisor;
+        this.cliente = cliente;
+        this.lineas = lineas;
+        this.observaciones = observaciones;
+        this.total = this.calculaTotal();
+        this.id = this.uniqueID();
+    }
+
+    Factura.prototype.uniqueID = (function() {
+        let id = 0; 
+        return function() { return id++; };  
+     })();
+
+    Factura.prototype.calculaTotal = function () {
+        let total = 0;
+        this.lineas.forEach(element => {
+            total += parseFloat(element.importe);
+        });
+        return total.toFixed(2);
+    };
 }
-
-function Linea(descripcionProducto, unidades, precio, iva, importe) {
-    this.descripcionProducto = descripcionProducto;
-    this.unidades = unidades;
-    this.precio = precio;
-    this.iva = iva;
-    this.importe = importe;
-}
-
-function Emisor(nombre, cif, direccion, telefono, email) {
-    this.nombre = nombre;
-    this.cif = cif;
-    this.direccion = direccion;
-    this.telefono = telefono;
-    this.email = email;
-}
-
-function Factura(fecha, moneda, emisor, cliente, lineas, observaciones) {
-    this.fecha = fecha;
-    this.moneda = moneda;
-    this.emisor = emisor;
-    this.cliente = cliente;
-    this.lineas = lineas;
-    this.observaciones = observaciones;
-    this.total = this.calculaTotal();
-    this.id = this.incrementaId();
-}
-
-Factura.prototype.id = 0;
-
-Factura.prototype.incrementaId = function () {
-    return this.id++;
-};
-
-// Factura.prototype.calculaTotal = function () {
-//     let arrayLineas = this.lineas;
-//     let total = 0;
-//     arrayLineas.forEach(element => {
-//         total += parseFloat(element.importe);
-//     });
-
-//     total += total * parseFloat(this.irpf / 100);
-//     total += total * parseFloat(this.re / 100);
-//     return total.toFixed(2);
-// };
