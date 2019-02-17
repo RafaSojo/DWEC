431e4e7c581f00e99268ef77214ef37bbfb912c7
Rafa Sojo <rafasojo98@gmail.com>
2019-02-17 13:26:49 +0100

    Cargar ficheros

---
+++ tema8/cargar-fichero/js/main.js
@@ -0,0 +1,47 @@
+{
+    let $estadoPeticion, $resultadoPeticion;
+    $(function (){
+        $estadoPeticion = $('#estadoPeticion');
+        $resultadoPeticion = $('#resultadoPeticion');
+        $('#cargar').click(function () {
+            let cadena = "";
+            $.get({
+                    url: $('#urlACargar').val(),    
+                    error: function () {
+                        cadena = "No Inicializada";
+                        $estadoPeticion.val(cadena);
+                    },
+                    success: function (data) {
+                        cadena += ", Éxito";
+                        $estadoPeticion.val(cadena);
+                        $("#scroller").text(data);
+                    },
+                    complete: function () {
+                        cadena += ", Completada";
+                        $estadoPeticion.val(cadena);
+                    },
+                    beforeSend: function () {
+                        cadena = "Antes de enviar";
+                        $estadoPeticion.val(cadena);
+                    }
+    
+    
+                }).done(function () {
+                    cadena += ", Realizado (done)";
+                    $estadoPeticion.val(cadena);
+                })
+                .fail(function () {
+                    cadena += ", Fallo (fail)";
+                    $estadoPeticion.val(cadena);
+                })
+                .always(function () {
+                    cadena += ", Finalizado (always)":
+                    $estadoPeticion.val(cadena);
+                })
+        
+        });
+    });
+
+
+    
+}
