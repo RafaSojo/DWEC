ef44e16491e06166bc954c030a26176bd938cfb6
Rafa Sojo <rafasojo98@gmail.com>
2019-02-25 09:14:41 +0100

    Corrección ejercicios

--- tema8/clasificaciones-json/js/main.js
+++ tema8/clasificaciones-json/js/main.js
@@ -1,29 +1,20 @@
 {
-    // $contenido = $("#contenido");
-// var a;
     function pintaContenido(data) {
-        // $contenido.html('');
-        // a = data;
         let arrayKeys = Object.keys(data);
         let html = '';
         for(let i=0;i<arrayKeys.length;i++){
             elemento = data[arrayKeys[i]];
             console.log(elemento);
             html += '<h1>' + arrayKeys[i] + '</h1>';
-            if (Array.isArray(elemento)) {
-                html += '<ul>';
+            if (Array.isArray(elemento)) 
                 elemento.forEach(elementoArray => html +='<input type="checkbox">' + elementoArray + '<br>');
-               html += '</ul>';
-            } else
+             else
                 html += '<p>' + elemento + '</p>';
             $("#contenido").html(html);
         }
-
     }
 
     function init() {
-
-
         $("#json1").click(function () {
             $.getJSON('https://rafasojo.github.io/DWEC/tema8/clasificaciones-json/json/habilidades-vida.json').done(data => pintaContenido(data));
         });
@@ -33,9 +24,6 @@
         $("#json3").click(function () {
             $.getJSON('https://rafasojo.github.io/DWEC/tema8/clasificaciones-json/json/tipos-desarrolladores.json').done(data => pintaContenido(data));
         });
-
     }
-
-
     $(init);
 }
