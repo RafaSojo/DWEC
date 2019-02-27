8283ac6ce2344cbaff7c43092528aa21cc5c5429
Rafa Sojo <rafasojo98@gmail.com>
2019-02-27 09:19:11 +0100

    Actualización ejercicio

--- tema8/clasificaciones-json/js/main.js
+++ tema8/clasificaciones-json/js/main.js
@@ -1,17 +1,29 @@
 {
     function pintaContenido(data) {
         let arrayKeys = Object.keys(data);
-        let html = '';
-        for(let i=0;i<arrayKeys.length;i++){
-            elemento = data[arrayKeys[i]];
-            console.log(elemento);
-            html += '<h1>' + arrayKeys[i] + '</h1>';
-            if (Array.isArray(elemento)) 
-                elemento.forEach(elementoArray => html +='<input type="checkbox">' + elementoArray + '<br>');
-             else
+        let $contenedor = $('<div></div>');
+        let $select = $('<select><option selected disabled> Selecciona una opción</option></select>');
+        for (let i = 0; i < arrayKeys.length; i++)
+            $select.append($('<option>' + arrayKeys[i] + '</option>').val(i));
+        
+        $contenedor.append($select);
+        $contenedor.append('<div id="elementos"></div>')
+
+        $select.on('change', function (e) {
+            let html = '';
+            let valor = $(e.target).val();
+            elemento = data[arrayKeys[valor]];
+
+            if (Array.isArray(elemento))
+                elemento.forEach(elementoArray => html += '<input type="checkbox">' + elementoArray + '<br>');
+            else
                 html += '<p>' + elemento + '</p>';
-            $("#contenido").html(html);
-        }
+            $('#elementos').html(html);
+
+        });
+        $select.change();
+        $("#contenido").html($contenedor);
+
     }
 
     function init() {
