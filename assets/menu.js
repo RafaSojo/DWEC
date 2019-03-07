34274591d6c3f0cd3874a0a2096c919b2b8e79db
Rafa Sojo <rafasojo98@gmail.com>
2019-03-07 09:35:57 +0100

    Desactivado menú

--- assets/menu.js
+++ assets/menu.js
@@ -1,10 +1,10 @@
-{
-    function botonAtras(){
-        document.getElementsByTagName('body')[0].innerHTML += '<a href="../index.html" class="float-boton-atras" id="botonAtras"><i class="fas fa-arrow-left my-float"></i></a>';
-    }
+// {
+//     function botonAtras(){
+//         document.getElementsByTagName('body')[0].innerHTML += '<a href="../index.html" class="float-boton-atras" id="botonAtras"><i class="fas fa-arrow-left my-float"></i></a>';
+//     }
 
-    window.addEventListener('load',botonAtras);
-}
-function cambiarEnlaceBoton(enlace){
-    document.getElementById('botonAtras').href=enlace;
-}
+//     window.addEventListener('load',botonAtras);
+// }
+// function cambiarEnlaceBoton(enlace){
+//     document.getElementById('botonAtras').href=enlace;
+// }
